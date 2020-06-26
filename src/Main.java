84220bf3729f5c4eeede7c3e31ab0a3cd10cdee3
makkkkkkkkks <gmaks@i.ua>
2020-06-26 10:36:39 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5000;
+        float b = 4000.1f;
+        long c = 500000;
+        byte d = 100;
+        double e = 800;
+        short f = 50;
+
+        System.out.println(a + b);
+        System.out.println(c - d);
+        System.out.println(e / f);
+        System.out.println(a * b);
+    }
+}
+
